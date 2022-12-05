03db198aea7cf9b655ee334a795dac9df4fb4420
cira_ubt20arm <teerawat.tongloy@gmail.com>
2022-12-05 12:06:09 +0700

    test

--- boostrap.sh
+++ boostrap.sh
@@ -12,4 +12,10 @@ then
     cd /tmp
     wget -O libROSProjectManager.so https://github.com/CiRA-AMI/cira-ros-qt-creator-plugin/raw/main/arm64/ubuntu_20.04/libROSProjectManager.so
     sudo cp -r -v libROSProjectManager.so /usr/lib/aarch64-linux-gnu/qtcreator/plugins/
+    
+    wget -O CiRA_CORE_Dev_Template.zip https://github.com/CiRA-AMI/cira-ros-qt-creator-plugin/raw/main/template/CiRA_CORE_Dev_Template.zip
+    unzip CiRA_CORE_Dev_Template.zip
+    cp -r CiRA_CORE_Dev_Template ~/
 fi
+
+echo "cira_ros_qt_creator_plugin install complete"
