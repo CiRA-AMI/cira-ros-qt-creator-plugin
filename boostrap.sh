9dfcc6d63880e29bf48d5d7d7593ecb4d971572a
cira_ubt20arm <teerawat.tongloy@gmail.com>
2022-12-05 11:59:07 +0700

    test

--- boostrap.sh
+++ boostrap.sh
@@ -5,3 +5,11 @@ echo $os_version
 
 arch=$(dpkg --print-architecture)
 echo $arch
+
+if  [[ "$os_version" == "Ubuntu 20.04"* ]] && [[ $arch == "arm64"* ]] ;
+then
+    sudo apt install qtcreator
+    cd /tmp
+    wget -O libROSProjectManager.so https://github.com/CiRA-AMI/cira-ros-qt-creator-plugin/raw/main/arm64/ubuntu_20.04/libROSProjectManager.so
+    sudo cp -r -v libROSProjectManager.so /usr/lib/aarch64-linux-gnu/qtcreator/plugins/
+fi
